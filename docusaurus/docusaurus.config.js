91ebb651762d82d6dd14f1c9eef3d7e61a76a22f
Jerry Lundström <lundstrom.jerry@gmail.com>
2024-05-23 14:03:56 +0200

    .

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -1,11 +1,13 @@
 export default {
-  title: "DNS Metrics Documentation",
-  url: "https://jelu.github.io",
-  baseUrl: "/dns-metrics",
-  presets: [["@docusaurus/preset-classic",
-  {
-    docs: true,
-    blog: false,
-  }]],
-  themes: [["docusaurus-json-schema-plugin", {}]],
+    title: "DNS Metrics Documentation",
+    url: "https://jelu.github.io",
+    baseUrl: "/dns-metrics",
+    presets: [["@docusaurus/preset-classic",
+    {
+        docs: {
+            sidebarPath: './sidebars.js',
+        },
+        blog: false,
+    }]],
+    themes: [["docusaurus-json-schema-plugin", {}]],
 };
